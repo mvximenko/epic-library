4cb70ea7b5a0acb90c7a5741d1e11e1b6be4d156
mvximenko <mr.mxmnk@gmail.com>
2021-09-29 19:24:37 +0900

    Update styles

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,16 +6,56 @@
   $: term.set(val);
 </script>
 
-<header>
-  <h1 class="heading">Epic Library {data.length}</h1>
+<header class="header">
+  <h1 class="heading">
+    Epic Library <span class="quantity">({data.length})</span>
+  </h1>
 
-  <input bind:value={val} type="text" id="searchInput" aria-label="Search" />
+  <input bind:value={val} class="search" type="text" aria-label="Search" />
 </header>
 
 <style lang="scss">
+  .header {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    margin: 1.25em 0;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (min-width: 600px) {
+      flex-wrap: nowrap;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    @media screen and (min-width: 1920px) {
+      font-size: 22.4px;
+    }
+  }
+
   .heading {
-    margin: 25px 0;
     color: #ccc;
-    position: relative;
+  }
+
+  .quantity {
+    font-size: 0.8em;
+  }
+
+  .search {
+    height: 2em;
+    max-width: 80%;
+    padding: 0 1em;
+    margin-bottom: 0.67em;
+    font-size: 1.2em;
+    color: #ccc;
+    background: #24282f;
+    border: 2px solid #707070;
+    border-radius: 10px;
+    outline: none;
+
+    @media screen and (min-width: 600px) {
+      margin-bottom: 0;
+    }
   }
 </style>
