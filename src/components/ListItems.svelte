c282f8ec0201beebd88fe209a42a9a0b00df9945
mvximenko <mr.mxmnk@gmail.com>
2021-10-15 18:25:10 +0900

    Update styles

--- src/components/ListItems.svelte
+++ src/components/ListItems.svelte
@@ -35,46 +35,14 @@
     height: 0;
     position: relative;
     padding-top: calc(215 / 460 * 100%);
-    overflow: hidden;
-    position: relative;
     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.8);
-    transform-origin: center top;
-    transform-style: preserve-3d;
-    transform: translateZ(0);
     transition: 0.3s;
-
-    &:after {
-      content: '';
-      z-index: 10;
-      width: 200%;
-      height: 100%;
-      top: -90%;
-      left: -20px;
-      opacity: 0.1;
-      position: absolute;
-      transform: rotate(45deg);
-      background: linear-gradient(
-        to top,
-        transparent,
-        #fff 15%,
-        rgba(255, 255, 255, 0.5)
-      );
-      transition: 0.3s;
-    }
 
     &:hover,
     &:focus,
     &:active {
       box-shadow: 0 8px 16px 3px rgba(0, 0, 0, 0.6);
-      transform: translateY(-3px) scale(1.05) rotateX(15deg);
-    }
-
-    &:hover:after,
-    &:focus:after,
-    &:active:after {
-      transform: rotate(25deg);
-      top: -40%;
-      opacity: 0.15;
+      transform: scale(1.05);
     }
   }
 
