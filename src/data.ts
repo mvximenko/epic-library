b96107c6706e1813639a1c7c5ba54d64b591e602
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2024-01-02 19:18:41 +0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,5 @@
 const data = [
+  { id: 1966900, title: '20 Minutes Till Dawn' },
   { id: 1139900, title: 'Ghostrunner' },
   { id: 742420, title: 'Saints Row' },
   { id: 1014140, title: 'Snakebird Primer' },
