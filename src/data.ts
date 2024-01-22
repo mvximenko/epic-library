bf6f76f7e61a8331c88445317181af7c9c053715
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2024-01-22 20:46:48 +0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,5 @@
 const data = [
+  { id: 269270, title: "LOVE" },
   { id: 1031460, title: "Sail Forth" },
   { id: 1088850, title: "Marvel's Guardians of the Galaxy" },
   { id: 1966900, title: '20 Minutes Till Dawn' },
