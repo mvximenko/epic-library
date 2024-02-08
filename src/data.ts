922e76ff356bc949c87270f966017cd123ccf835
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2024-02-08 19:06:53 +0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,5 @@
 const data = [
+  { id: 1388880, title: "Doki Doki Literature Club Plus!" },
   { id: 300570, title: "Infinifactory" },
   { id: 269270, title: "LOVE" },
   { id: 1031460, title: "Sail Forth" },
